696b8c0b4e9f9898493c1024d01170f7d9ff1da8
otegami <a.s.takuya1026@gmail.com>
2020-06-26 21:34:37 +0900

    first commit

---
+++ practice/HTTP/get.rb
@@ -0,0 +1,22 @@
+GET /~joe/prog/cgi/env01.cgi HTTP/1.0
+User-Agent: Telnet [ja] (Linux)
+Host: dummy-bootcamp-fjord-jp.herokuapp.com
+
+GET /articles HTTP/1.0
+Host: dummy-bootcamp-fjord-jp.herokuapp.com
+
+GET /articles/new HTTP/1.0
+Host: dummy-bootcamp-fjord-jp.herokuapp.com
+
+POST /articles HTTP/1.0
+Host: dummy-bootcamp-fjord-jp.herokuapp.com
+Accept-Language: ja
+Accept-Encoding: gzip, deflate
+Content-Length: 80
+Connection: Keep-Alive
+Cache-Control: no-cache
+
+article[title]=Post Method&article[body]=Hello, This is the first time!
+
+
+User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)
